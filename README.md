61704698718249f2437f9547888e0fcc4b465f20
DidierLeBail <didierm.lebail@gmail.com>
2026-06-27 22:28:37 +0200

    minor

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ To show employers I have the ML skills they are looking for.
 
 This repo implements some of the Machine Learning projects described [here](https://www.dataquest.io/blog/machine-learning-projects-for-beginners-to-advanced/).
 
-## Beginner project: spam classifier
-The goal here is to detect automatically an email as being a spam or not.
-We extend the project by adding a third category, corresponding to the model being unsure whether the email is a spam.
-These emails are the only ones that should be inspected manually.
+# Beginner project: spam classifier
+The goal here is to detect automatically an SMS as being a spam or not.
+We extend the project by adding a third category, corresponding to the model being unsure whether the SMS is a spam.
+These SMS are the only ones that should be inspected manually.
 
+## Dataset
+The SMS dataset can be downloaded [here](https://archive.ics.uci.edu/dataset/228/sms+spam+collection).
+It does not have any missing values.
+After unzipping, the `.txt` file `SMSpamCollection` was moved to the `data` directory.
--- spamClassifier/main.py
+++ spamClassifier/main.py
@@ -1,1 +1,63 @@
+import pandas as pd
+import numpy as np
+import os
 
+datapath = os.path.join(
+    os.path.dirname(__file__), "data", "SMSSpamCollection.txt"
+)
+
+# For some reason, this line did not work (5572 rows instead of 5574)
+# dataset = pd.read_csv(datapath, sep='\t', names=("label", "text"))
+# so i ended up loading the data with open into a dictionary
+
+dataset = {"label": [], "text": []}
+with open(datapath, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        label, text = line.rstrip('\n').split('\t')
+        dataset["label"].append(label)
+        dataset["text"].append(text)
+dataset = pd.DataFrame(dataset)
+
+print(dataset.head())
+print(dataset.tail())
+print()
+
+print(f"nb of samples: {len(dataset.index)}, should be 5574")
+assert len(dataset) == 5574
+print()
+
+class SpamClassifier:
+    """A classifier that, given a SMS returns its probability of being a spam or a regular (ham) message.
+    
+    A SMS here is converted into a vector-valued time series (embedding).
+    The multinomial Bayesian classification model will then be independent from the time ordering of these series.
+    """
+
+    def __init__(self):
+        pass
+    
+    def fit(self, X_train: np.ndarray, Y_train: np.ndarray):
+        """Fits the model parameters from the training examples `X_train` and
+        their labels `Y_train`.
+        """
+
+    def __call__(self, msgs: np.ndarray):
+        pass
+
+def inspect_data(dataset: pd.DataFrame):
+    """Computes some basic statistics about `dataset`:
+    - number of representatives in each class
+    - message length when separating words with spaces
+    
+    This allows us to set some hyperparameters of our model, such as the number of tokens it should process at once.
+    """
+    label_count = {}
+    labels = dataset["label"].unique()
+    print(len(dataset))
+    for label in labels:
+        label_count[label] = sum(dataset["label"] == label)
+    print(label_count)
+
+inspect_data(dataset)
+print(4825 + 747)
